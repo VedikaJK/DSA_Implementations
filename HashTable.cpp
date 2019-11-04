1b0dfc88f09b11d3fef1bb71b41bf119e1ac4a6a
VedikaJK <50884920+VedikaJK@users.noreply.github.com>
2019-11-04 13:13:45 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -18,11 +18,13 @@ int i;
 class HashTable{
      private:
      string table[SIZE];
+     int occupied[SIZE];
      int capacity;
      public:
      HashTable(){
          fo(i,SIZE){
              table[i]=" ";
+             occupied[i]=0;
          }
          capacity=0;
      }
@@ -30,17 +32,18 @@ class HashTable{
      void displayHT();
      void insert(string s);
     void search(string s);
+    void delete_(string s);
 
  };
 
 int main(){
     HashTable ht;
-    cout<<" 1 for Insert, enter String also \n 2 for Search, enter String also \n 3 to Display\n 4 to stop \n ";
+    cout<<" 1 for Insert, enter String also \n 2 for Search, enter String also \n 3 to Display\n4 Delete , enter string too\n 5 to stop \n ";
     cout<<"Enter queries:\n";
     while(1){
         int a;
         cin>>a;
-        if(a==4) break;
+        if(a==5) break;
         string s;
         switch(a){
             case 1:
@@ -53,6 +56,10 @@ int main(){
             break;
             case 3:
             ht.displayHT();
+            break;
+            case 4:
+            cin>>s;
+            ht.delete_(s);
         }
         
     }
@@ -70,16 +77,20 @@ int HashTable::getHash(string s){
 
 void HashTable::displayHT(){
     fo(i,SIZE){
+        if(occupied[i]!=0)
         cout<<i<<"\t"<<table[i]<<"\n";
     }
+    cout<<"done\n";
+    cout<<endl;
 }
 
 void HashTable::insert(string s){
     int hashIndex=getHash(s);
-    if(capacity>=SIZE) cout<<"Table is full. Cannot insert.";
+    if(capacity>=SIZE) cout<<"Table is full. Cannot insert.\n";
     else{
-        while(table[hashIndex]!=" ")  hashIndex=(hashIndex+1)%SIZE;
+        while(table[hashIndex]!=" "||(occupied[hashIndex]!=0))  hashIndex=(hashIndex+1)%SIZE;
         table[hashIndex]=s;
+        occupied[hashIndex]=1;
         capacity++;
     }
     //cout<<s<<" inserted\n";
@@ -89,9 +100,45 @@ void HashTable::insert(string s){
 
 void HashTable::search(string s){
     int hashIndex=getHash(s);
-    while((table[hashIndex]!=s)&&(table[hashIndex]!=" ")){
+    int p=hashIndex,flag=0;
+    while(1){
+        int index=getHash(table[hashIndex]);
+        if(index!=p) {flag=1; cout<<s<<" not found.\n"; return;}
+        if((table[hashIndex]==s)&&(occupied[hashIndex]==1)){
+            cout<<s<<" found.\n"; return;
+        }
+        if((table[hashIndex]==s)&&(occupied[hashIndex]==0)){
+            cout<<s<<" not Found.\n"; return;
+        }
+        if(table[hashIndex]==" "){
+            cout<<s<<" not found.\n"; return;
+        }
         hashIndex=(hashIndex+1)%SIZE;
     }
-    if(table[hashIndex]==" ") cout<<s<<" not found.\n";
-    else cout<<s<<" is found.\n";
+    
 }
+
+void HashTable::delete_(string s){
+
+    int hashIndex=getHash(s);
+    int p=hashIndex,flag=0;
+    while(1){
+        int index=getHash(table[hashIndex]);
+        if(index!=p) { return;}
+        if((table[hashIndex]==s)&&(occupied[hashIndex]==1)){
+            occupied[hashIndex]=0; return;
+        }
+        if((table[hashIndex]==s)&&(occupied[hashIndex]==0)){
+            return;
+        }
+        if(table[hashIndex]==" "){
+             return;
+        }
+        hashIndex=(hashIndex+1)%SIZE;
+    }
+    
+}
+
+
+
+
